f6087d5249174638b99149ab6cb8a6bd787d674a
xsiomchanka <xsiomchanka@stuba.sk>
2026-01-05 15:15:46 +0100

    liza first fix

--- my-game/dev-dist/sw.js
+++ my-game/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lj6qaku5q5o"
+    "revision": "0.laad73du16g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
