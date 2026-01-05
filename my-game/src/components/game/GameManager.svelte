b283f102de7c88672e0fc4b57fce7e5ddf981a8e
xsiomchanka <xsiomchanka@stuba.sk>
2026-01-05 14:22:13 +0100

    lvl sequence fixed

--- my-game/src/components/game/GameManager.svelte
+++ my-game/src/components/game/GameManager.svelte
@@ -9,7 +9,6 @@
   import LevelPassword from "./levels/LevelPassword.svelte";
   import LevelCrossRoad from "./levels/LevelCrossRoad.svelte";
   import LevelMemory from "./levels/LevelMemory.svelte";
-  import UnknownLevel from "./levels/UnknownLevel.svelte";
 
   $: currentLevelData = $levels[$currentLevelIndex];
   let levelComplete = false;
@@ -82,7 +81,6 @@
           case "password": return LevelPassword;
           case "traffic-light": return LevelCrossRoad;
           case "memory": return LevelMemory;
-          default: return UnknownLevel;
       }
   }
 
